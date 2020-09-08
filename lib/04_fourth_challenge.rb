45c66244483ff26e9151519f579037b6c07bb0dc
Kevin Glasgow <kglas001@gmail.com>
2020-09-08 00:47:46 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -1,5 +1,5 @@
 def fourth_challenge
-     montague => {
+     montague: {
       patriarch: {name: "Lord Montague", age: "53"},
       matriarch: {name: "Lady Montague", age: "54"},
       hero: {name: "Romeo", age: "15", status: "alive"},
@@ -8,7 +8,7 @@ def fourth_challenge
          {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    },
-   capulet => {
+   capulet: {
       patriarch: {name: "Lord Capulet", age: "50"},
       matriarch: {name: "Lady Capulet", age: "51"},
       heroine: {name: "Juliet", age: "15", status: "alive"},
